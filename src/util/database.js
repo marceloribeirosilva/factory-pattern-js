f62d812a6338bbc8c2f003f06f62ada78a392ce7
marceloribeirosilva <ribmarc@gmail.com>
2021-03-04 23:49:56 -0300

    create database fake

---
+++ src/util/database.js
@@ -0,0 +1,21 @@
+export default class Database {
+  constructor({ connectionString }) {
+    this.connectionString = connectionString;
+  }
+
+  async sleep(ms) {
+    return new Promise(resolve => {
+      setTimeout(resolve, ms);
+    });
+  }
+
+  async connect() {
+    await this.sleep(100);
+    return this;
+  }
+
+  async find(query) {
+    await this.sleep(100);
+    return [{name:'Marcelo Silva'}];
+  }
+}
