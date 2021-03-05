2fe7d39d27fd1e9b7b54a367f64a8c8c433754c7
marceloribeirosilva <ribmarc@gmail.com>
2021-03-05 00:00:09 -0300

    create user service

---
+++ src/service/userService.js
@@ -0,0 +1,11 @@
+export default class UserService {
+  constructor({ userRepository }) {
+    this.userRepository = userRepository;
+  }
+
+  async find(query) {
+    const users = await this.userRepository.find(query);
+
+    return users.map(user => ({... user, name: user.name.toUpperCase()}));
+  }
+}
